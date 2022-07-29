03e92f12c47c2f88241d66c425531a1890e443e3
musawakiliML <musawakiliml@hotmail.com>
2022-07-30 00:49:58 +0100

    Added pages and templates

--- pages_app/views.py
+++ pages_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
+# function-based views
+# def homePageView(request):
+#   return HttpResponse("Hello, World!")
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+# Class Based view
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
