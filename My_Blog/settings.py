cc13d4aac0033a64804d8a3d725c1990527f78af
musawakiliML <musawakiliml@hotmail.com>
2022-07-29 23:43:01 +0100

    Added a pages_app

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My Apps
+    'pages_app.apps.PagesAppConfig',
+
+    # Admin Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
