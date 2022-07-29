80cf8c77874dd337406e2d9cf58fa1e391320a10
musawakiliML <musawakiliml@hotmail.com>
2022-07-30 00:37:39 +0100

    Create template at project level

--- My_Blog/settings.py
+++ My_Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,8 @@ ROOT_URLCONF = 'My_Blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # new project-level templating
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
