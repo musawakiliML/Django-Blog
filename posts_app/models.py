db88983a94ea559252767897dea6afc82f4e3dbd
musawakiliML <musawakiliml@hotmail.com>
2022-08-04 02:37:53 +0100

    models and admin dashboard update

--- posts_app/models.py
+++ posts_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    '''This Model stores the textual content of our posts'''
+    text = models.TextField()
+    
